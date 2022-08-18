95266a672977dd4dae4cdfaad87cdd91a1b9a0fa
Amer_Hamwi <amerhamwi58@gmail.com>
2022-08-18 13:31:26 +0300

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,24 @@
+import "./bootstrap";
+
+/* Set up using Vue 3 */
+import { createApp } from 'vue'
+import App from './Page/app.vue'
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faPlusSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faPlusSquare, faTrash)
+
+createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
+
+
+
