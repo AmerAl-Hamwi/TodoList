2cca60380e664782e2b6d18c2ec790629ef8dc6a
Amer_Hamwi <amerhamwi58@gmail.com>
2022-08-24 15:40:53 +0300

    Edit create button

--- resources/js/Page/addItemForm.vue
+++ resources/js/Page/addItemForm.vue
@@ -1,79 +1,91 @@
 <template>
-    <div class="addItem">
-        <input type="text" v-model="item.name" />
-        <font-awesome-icon
-            class="button"
-            icon="plus-square"
-            @click="addItem()"
-            :class="[item.name ? 'active' : 'inactive', 'plus']"
-        />
-    </div>
+  <div class="addItem">
+    <input type="text" v-model="item.name" />
+    <font-awesome-icon
+      class="button"
+      icon="plus-square"
+      @click="addItem()"
+      :class="[item.name ? 'active' : 'inactive', 'plus']"
+    />
+  </div>
 </template>
 
 <script>
 import axios from "axios";
 
 export default {
-    data: function () {
-        return {
-            item: {
-                name: "",
-            },
-        };
+  data: function () {
+    return {
+      item: {
+        name: "",
+      },
+    };
+  },
+  methods: {
+    addItem() {
+      if (this.item.name == "") {
+        return;
+      }
+
+      axios
+        .post("api/item", {
+          name: this.item.name,
+        })
+
+        .then((Response) => {
+          if (Response.data.status == 201) {
+            this.item.name == "";
+          }
+        })
+
+        .catch((error) => {
+          console.log(error);
+        });
     },
-    methods: {
-        addItem() {
-            if (this.item.name == "") {
-                return;
-            }
-
-            axios
-                .post("api/item", {
-                    name: this.item.name,
-                })
-
-                .then((Response) => {
-                    if (Response.data.status == 201) {
-                        this.item.name == "";
-                    }
-                })
-
-                .catch((error) => {
-                    console.log(error);
-                });
-        },
-    },
+  },
 };
 </script>
 
 <style scoped>
 .addItem {
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 input {
-    background-color: #f7f7f7;
-    border: 0px;
-    outline: none;
-    padding: 5px;
-    margin-right: 10px;
-    width: 100%;
+  background-color: #f7f7f7;
+  border: 0px;
+  outline: none;
+  padding: 5px;
+  margin-right: 10px;
+  width: 100%;
 }
 
 .button {
-    font-size: 20px;
+  font-size: 20px;
 }
 .plus {
-    font-size: 20px;
+  font-size: 20px;
 }
 
 .active {
     color: #00ce25;
+  cursor: pointer;
+  
+}
+
+.active:hover {
+    color: #01901b;
+}
+
+.active:active {
+    color: #000;
 }
 
 .inactive {
-    color: #999999;
+  color: #999999;
 }
 </style>
+
+font-awesome-icon { }
